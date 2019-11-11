5d232a9821032ce284a3a0a690614891d8d9253e
Ubuntu <ubuntu@ip-172-31-81-170.ec2.internal>
2019-11-11 16:39:09 +0000

    portfolio component initialised with content

--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -6,6 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  
+  subNav = [
+    {name: 'HTML & CSS'},
+    {name: 'JavaScript'},
+    {name: 'Python with Flask'},
+    {name: 'Python with Django'},
+    {name: 'Angular'},
+    ]
+  
+  displayed="HTML & CSS"
+  
+  isActive="yes"
+  
+  changeView(info){
+    this.displayed= info
+    console.log(this.displayed)
+  }
 
   constructor() { }
 
