2d2f8ee14116e4f1b01e35ac343a4d493f72cb25
Ubuntu <ubuntu@ip-172-31-81-170.ec2.internal>
2019-11-11 16:31:03 +0000

    initial content added for tech-stack component

--- portfolio/src/app/tech-stack/tech-stack.component.ts
+++ portfolio/src/app/tech-stack/tech-stack.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tech-stack.component.css']
 })
 export class TechStackComponent implements OnInit {
+  
+  subNav = [
+    {name: 'HTML'},
+    {name: 'CSS'},
+    {name: 'Bootstrap/Bootwatch'},
+    {name: 'jQuery'},
+    {name: 'JavaScript'},
+    {name: 'API Interaction'},
+    {name: 'JSON Representation'},
+    {name: 'Python'},
+    {name: 'Flask'},
+    {name: 'Django'},
+    {name: 'SQL'},
+    {name: 'Stripe'},
+    {name: 'MongoDB'},
+    ]
+  
+  displayed="HTML"
+  
+  isActive="yes"
+  
+  changeView(info){
+    this.displayed= info
+    console.log(this.displayed)
+  }
 
   constructor() { }
 
